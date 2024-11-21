f6df9bd71a14f3453bfb5d1d1229bea17d86b672
D <40243930+danish90@users.noreply.github.com>
2024-11-20 19:56:48 -0800

    SlotMachine_project.py

--- Slot_Machine.py
+++ Slot_Machine.py
@@ -7,11 +7,6 @@ def spin_row():
 
     return [random.choice(symbols) for _ in range(3)]
 
-    # results = []
-    # for symbol in range(3):
-    #     results.append(random.choice(symbols))
-    # return results
-
 def print_row(row):
     print("*************")
     print(" | ".join(row))
